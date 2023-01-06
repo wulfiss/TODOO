3ba4c670f92f9353f07191b033008e3ced76decc
Julian <julian_wv@live.com.ar>
2023-01-06 00:11:44 -0300

    Initial setup

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import './style.scss';
+
+const lala = 'lalala';
+
+console.log(lala);
---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    output:{
+        filename:'index_bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+    },
+    plugins: [new HtmlWebpackPlugin({
+        title: 'TO-DO',
+    })],
+    devtool:'inline-source-map',
+    mode: 'development',
+    module:{
+        rules:[
+            {
+            test: /\.s[ac]ss$/i,
+            use:[
+                "style-loader",
+                "css-loader",
+                "sass-loader",
+                ],
+            },
+        ],
+    },
+};
