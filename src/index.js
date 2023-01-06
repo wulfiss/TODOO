dcd212b41e629b8983ee65632a09bc4798cd1001
Julian <96155980+wulfiss@users.noreply.github.com>
2023-01-06 02:30:16 -0300

    Add form

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import './style.scss';
+import userForm from './modules/ui';
 
+
+const body = document.querySelector('body');
+body.appendChild(userForm());
 
 const lala = 'What a shit';
 
--- src/modules/ui.js
+++ src/modules/ui.js
@@ -27,5 +27,58 @@ function btnCreator(id, btnClass, text){
 
   return btn;
 }
+// ++++++++++++++++++++++//
 
+const userForm = () => {
+
+  const form = document.createElement('form');
+
+  const inputTitle = createEl('text','inputTitle', 'input');
+  const labelInputTitle = labelCreator('inputTitle', 'Title: ');
+
+  form.appendChild(labelInputTitle);
+  form.appendChild(inputTitle);
+
+  /* const inputDescription = createEl('text', 'inputDescrip', 'input');
+  const labelInputDescrip = labelCreator ('inputDescrip', 'Description: ');
+
+  form.appendChild(labelInputDescrip);
+  form.appendChild(inputDescription);
+  */
+
+  const inputDate = createEl('date', 'inputDate', 'input');
+  const labelInputDate = labelCreator('inputDate', 'Due: ');
+
+  form.appendChild(labelInputDate);
+  form.appendChild(inputDate);
+
+  // create a select element and add elements to the dropdown
+  const inputPriority = createEl('select','inputPriority', 'select');
+  const inputPriorityLabel = labelCreator('inputPriority', 'Priority');
+
+  const optArr = ['Low', 'Medium', 'High'];
+
+  for(let i = 0; i < optArr.length; i++){
+    const options = createEl('option','', 'option');
+    options.value = optArr[i];
+    options.id = optArr[i];
+    const txtNode = document.createTextNode(optArr[i]);
+
+    options.appendChild(txtNode);
+    inputPriority.appendChild(options);
+  }
+
+  form.appendChild(inputPriorityLabel);
+  form.appendChild(inputPriority);
+
+  const btnAdd = btnCreator('btnAdd', 'btnShow', 'Add');
+  const btnCancel = btnCreator('btnCancel', 'btnShow', 'Cancel');
+
+  form.appendChild(btnAdd);
+  form.appendChild(btnCancel);
+
+  return form;
+}
+
+export default userForm;
 
